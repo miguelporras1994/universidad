bcfcbcb2073779d3a969d8f0eb8aad5ecb5a3d7a
Miguel porras <porraangel@hotmail.com>
2019-05-27 07:31:26 -0500

    valido que paso code error en javascript

--- universidad/wwwroot/js/Categorias.js
+++ universidad/wwwroot/js/Categorias.js
@@ -6,7 +6,11 @@
         this.descripcion = descripcion;
         this.estado = estado;
         this.action = action;
+
     }
+
+
+
 
     GuardarCategoria() {
         if (this.id == "") {
@@ -18,46 +22,49 @@
             document.getElementById("Nombre").focus();
 
         } else {
-            if (this.descripcion =="") {
+            if (this.descripcion == "") {
                 document.getElementById("Descripcion").focus();
             } else {
-                alert(this.nombre);
-                alert(this.id);
-                alert(this.action);
 
                 var id = this.id;
                 var nombre = this.nombre;
                 var descripcion = this.descripcion;
                 var estado = this.estado;
                 var action = this.action;
+                var mensaje ='';
                 $.ajax({
                     type: "POST",
                     url: action,
                     data: {
                         id, nombre, descripcion, estado
                     },
-                    success: function (response) {
-                        if (Response == "Save") {
-                            window.location.href = "Categoria"
+                    success: (response) => {
+                        $.each(response, (index, val) => {
+                            mensaje = val.code;
+                        });
+
+
+                        if (mensaje == "SAVE") {
+                            this.restablecer();
+                        } else {
+
+                            document.getElementById("mensaje").innerHTML = "no se pudo guardar la categoria "
 
                         }
-                        else {
-                            alert("no funciona ")
-
-                        }
-                           
-
                     }
 
+                    
                 });
 
-
-
             }
-
-
-
         }
+    }
 
+    restablecer() {
+        document.getElementById("Nombre").value = "";
+        document.getElementById("Descripcion").value = "";
+        document.getElementById("mensaje").innerHTML = "";
+        document.getElementById("Estado").selectedIndex = 0;
+        $('#modalAC').modal('hide');
     }
 }
